d844ad19cdf416e940538518e10cdb53ce777370
41SergioF <carlossergio2401@outook.com>
2021-12-02 14:25:42 -0300

    Desenvolvendo o BD - (Comentários)

--- repository.mongodb/src/main/java/br/com/carlosfernandes/repository/mongodb/connections/MongoTemplateSingleton.java
+++ repository.mongodb/src/main/java/br/com/carlosfernandes/repository/mongodb/connections/MongoTemplateSingleton.java
@@ -6,10 +6,14 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 
 public class MongoTemplateSingleton {
 
+    /// Impede que a classe seja instanciada
     private MongoTemplateSingleton() {}
 
+    /* Esse variárel se torna uma constante que salva um instancia de conexão
+     * com o Banco de dados */
     public static final MongoTemplate operations;
 
+    /// A conexão é feita nas linhas abaixo
     static {
         // Esse trexo cria a conecxão com o bando de dados
         var client = MongoClients.create("mongodb://localhost:27017/reserva_mongo");
